2cfdebed188c4f2970234bb3a15d61e2687cc7b6
Murilo Oliveira <murilonobrega8@gmail.com>
2022-09-13 01:10:37 -0300

    created_ProjetoPessoas

---
+++ Aula11ProjetoPessoas2/src/aula11projetopessoas2/Aula11ProjetoPessoas2.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula11projetopessoas2;
+
+/**
+ *
+ * @author murilonobrega8
+ */
+public class Aula11ProjetoPessoas2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Pessoa p1 = new Pessoa();
+        Aluno p2 = new Aluno();
+        Professor p3 = new Professor();
+        Funcionario p4 = new Funcionario();
+        
+        p1.setNome("Pedro");
+        p2.setNome("Maria");
+        p3.setNome("Cláudio");
+        p4.setNome("Fabiana");
+        
+        p1.setSexo("M");
+        p4.setSexo("F");
+        p2.setIdade(18);
+        
+        p2.setCurso("Informática");
+        p3.setSalario(2500.75f);
+        p4.setSetor("Estoque");
+        
+        System.out.println(p1.toString());
+        System.out.println(p2.toString());
+        System.out.println(p3.toString());
+        System.out.println(p4.toString());
+    }
+    
+}
